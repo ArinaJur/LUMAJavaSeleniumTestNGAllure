eb901d2a91cae7799bd94b6704da5383f099b92c
ArinaJur <66705803+ArinaJur@users.noreply.github.com>
2024-05-31 14:51:03 -0400

    Allure reports (#6)
    * add Allure to the project

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -1,8 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class TestTest {
     @Test
-    public void test() {
+    public void testPass() {
 
+        Assert.assertTrue(true);
+    }
+
+    @Ignore
+    @Test
+    public void testFail() {
+
+        Assert.assertTrue(false);
     }
 }
